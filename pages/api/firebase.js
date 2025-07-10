e246816048ede396783572139027cc681bb3d376
Yogaaa26 <yogasptraaa26@gmail.com>
2025-07-10 20:05:55 +0700

    Update firebase.js

--- pages/api/firebase.js
+++ pages/api/firebase.js
@@ -1,14 +1,16 @@
+// pages/api/firebase.js
 import admin from "firebase-admin";
-
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: process.env.FIREBASE_DATABASE_URL
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    }),
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
   });
 }
 
 const db = admin.database();
-
-export { db };
+export { admin, db };
